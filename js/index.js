b868a06ae9b1d496a58d6b54d879a48ab3e23625
junob <805517373@qq.com>
2016-07-23 09:55:47 +0800

    sofa

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2016/5/24.
+ */
+$(function(){
+    $(".menu").click(function(){
+        $(".slidemenu").slideToggle();
+    })
+    // $(".znds-item").find(".item").hover(function(){
+    //     $(this).find(".item-con").toggleClass("active")
+    // },function(){})
+})
